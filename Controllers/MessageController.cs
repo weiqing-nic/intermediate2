3ab87af4967b5c228394082006d4c9f3125b33bc
weiqing-nic <yoweiqing@gmail.com>
2021-11-15 15:45:26 +0800

    Add files via upload
    first version http get will run the 15 minutes  api. the thread will sleep for 15 minutes, then run again

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using producerd.Models;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace producerd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public void getbpi() 
+        {
+            int i2 = 0;
+            while (true)
+            {
+                if (i2 == 0)
+                {
+                    i2 += 1;
+                    var factory = new ConnectionFactory()
+                    {
+                        //HostName = "host.docker.internal"
+                        //Port = "31859"
+                        //                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+                        //              Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
+                        HostName = "localhost",
+                        Port = 31672
+                    };
+                    int i = 0;
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "Bpi",
+                                   durable: false,
+                                   exclusive: false,
+                                   autoDelete: false,
+                                   arguments: null);
+
+                        var reponsestring = "https://api.coindesk.com/v1/bpi/currentprice.json";
+                        var client = new HttpClient();
+                        var responseresult = client.GetAsync(reponsestring);
+                        responseresult.Wait();
+                        var result = responseresult.Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            var readAsync = result.Content.ReadAsStringAsync();
+                            readAsync.Wait();
+                            string s = readAsync.Result;
+                            readAsync.Wait();
+    //                        int index2 = s.IndexOf("bpi") + 5;
+  //                          int nlen = s.Length - index2 - 1;
+//                            string news = s.Substring(index2, nlen);
+                            string message = "hello";
+                            var body = Encoding.UTF8.GetBytes(s);
+                            channel.BasicPublish(exchange: "",
+                                         routingKey: "Bpi",
+                                         basicProperties: null,
+                                         body: body);
+                        }
+
+
+                    }
+                }
+                else
+                {
+                    var factory = new ConnectionFactory()
+                    {
+                        //HostName = "host.docker.internal"
+                        //Port = "31859"
+                        //                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+                        //              Port = Convert.ToInt32(Environment.GetEnvironmentVariable("RABBITMQ_PORT"))
+                        HostName = "localhost",
+                        Port = 31672
+                    };
+                    int i = 0;
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+
+                        var reponsestring = "https://api.coindesk.com/v1/bpi/currentprice.json";
+                        var client = new HttpClient();
+                        var responseresult = client.GetAsync(reponsestring);
+                        responseresult.Wait();
+                        var result = responseresult.Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            var readAsync = result.Content.ReadAsStringAsync();
+                            readAsync.Wait();
+                            string s = readAsync.Result;
+                            readAsync.Wait();
+                            //                        int index2 = s.IndexOf("bpi") + 5;
+                            //                          int nlen = s.Length - index2 - 1;
+                            //                            string news = s.Substring(index2, nlen);
+                            string message = "hello";
+                            var body = Encoding.UTF8.GetBytes(s);
+                            channel.BasicPublish(exchange: "",
+                                         routingKey: "Bpi",
+                                         basicProperties: null,
+                                         body: body);
+                        }
+
+
+                    }
+                }
+                int timesleep = 15 *   60  * 1000;
+                Thread.Sleep(timesleep); //10 seconds
+
+            }
+        }
+        [HttpPost]
+        public async Task<Message> Post( Message greeting)
+        {
+
+            var json = JsonConvert.SerializeObject(greeting);
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "https://httpbin.org/post";
+            var client = new HttpClient();
+
+            var responseresult = await client.PostAsync(url, data);
+
+            if (responseresult.IsSuccessStatusCode)
+            {
+                Console.WriteLine("heele");
+                var readAsync = responseresult.Content.ReadAsStringAsync();
+                string s = readAsync.Result;
+                readAsync.Wait();
+                Console.WriteLine(readAsync);
+                Console.WriteLine("hdsdfhdsfhsdfh");
+                greeting.Greet = s;
+                return greeting;
+            }
+            else
+            {
+                Console.WriteLine("fail");
+            }
+
+
+            return greeting;
+        }
+
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace producerd.Models
+{
+    public class Message
+    {
+        public string Greet { get; set; }
+    }
+}
